808772c21f3417da66de2ceeaaca13fb08d4b679
charlesarchibong <charlesarchibong10@gmail.com>
2021-03-05 14:30:46 +0100

    added app mixing to the http_service_requester

---
+++ lib/core/network/http_service_requester.dart
@@ -0,0 +1,42 @@
+import 'package:dio/dio.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter_credo/core/constant/credo_constants.dart';
+
+class HttpServiceRequester with CredoConstants {
+  Dio dio;
+  HttpServiceRequester({
+    @required this.dio,
+  }) {
+    this.dio = Dio();
+  }
+
+  Future<Response> post({
+    @required String endpoint,
+    String secretKey,
+    dynamic body,
+    Map queryParam,
+  }) async {
+    dio.options.headers = headers;
+    dio.options.headers["Authorization"] = "Bearer $secretKey";
+    Response response = await dio.post(
+      baseUrl + endpoint,
+      data: body,
+      queryParameters: queryParam,
+    );
+    return response;
+  }
+
+  Future<dynamic> getRequest({
+    @required String endpoint,
+    @required String secretKey,
+    @required Map queryParam,
+  }) async {
+    dio.options.headers = headers;
+    dio.options.headers["Authorization"] = "Bearer $secretKey";
+
+    Response response = await dio.get(
+      baseUrl + endpoint,
+    );
+    return response;
+  }
+}
